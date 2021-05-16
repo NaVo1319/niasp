78416c8e8e8a1da5b156b655b2a5bde21f9cc3ea
Nazarkin V.I <vohammer2001@gmail.com>
2021-05-16 15:03:52 +0300

    fix

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
-var express = require('express');
-var app = express();
+const express = require('express')
+const app = express()
+const port = 3000
 
-app.set('port',(process.env.PORT || 5000));
-app.use(express.static(__dirname + '/public'));
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-
-app.get('/', function(request, response){
-  response.render('pages/index')
-});
-
-app.listen(app.get('port'), function() {
- console.log('Node app is running on port', app.get('port'));
-});
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
